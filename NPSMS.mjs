0bf7378c4bac3e8e19c021fb27049eecd11c2473
fawkeso16 <64199725+fawkeso16@users.noreply.github.com>
2024-05-29 20:06:15 +0100

    Update and rename NPSMS.js to NPSMS.mjs
    changed file type to mjs, reason needed to use import modules rather than common (idk why but it now works so no worry). added comments so you guys can follow along with some of the code if needed

--- NPSMS.mjs
+++ NPSMS.mjs
@@ -1,23 +1,21 @@
-const fetch = require('node-fetch')
+import fetch from 'node-fetch';
 
-const URL = 'api.openweathermap.org/data/2.5/forecast?lat=12.57&lon=106.9&appid=331e13a2241a062eec05fdd320fad13b'
+//const URL = 'api.openweathermap.org/data/2.5/forecast?lat=12.57&lon=106.9&appid=331e13a2241a062eec05fdd320fad13b'
 //code to request data using API weather
 
 
 
 //function to determine if weather is extreme
-function weatherupdate(URL, ){
+//function weatherupdate(URL, ){
 
 }
-function extremeWeather(){
-    if (blablabla){
-        return true;
-    }
-}
-;
+//function extremeWeather(){
+//     if (blablabla){
+//         return true;
+//     }
+// }
+// ;
 
-
-// this is ollies authentication data, we only need one persons.
 
 const myPhoneNumber = '447401105231';
 const jamiePhoneNumber = '447575202880';
@@ -25,15 +23,13 @@ const tyronePhoneNumber = '447939105014';
 const norbertPhoneNumber = '447592797548';
 
 
-
-message = 'This is a test, hopefully this works';
-
-
 const message = 'This is a test, hopefully this works';
 
 
-//chnaged sms api service to clicksend, this async function 
+//changed sms api service to clicksend, this async function takes in the phone number and message.
+// we have 2 options loop for every phone number, and send a message. or send a message to everyone at once (this would be harder i believe)
 const sendSms = async (myPhoneNumber, message) =>{
+    //ollie authentiation details
     const username = 'ollieSMS';
     const apiKey = '4B1D7004-C2C2-2CEA-D49E-5C8406E03346'
 
@@ -56,6 +52,9 @@ const sendSms = async (myPhoneNumber, message) =>{
     },
     body: JSON.stringify(data)
     });
+    
+
+    //error checking
 
     if(!response.ok){
         const error = await response.json();
@@ -67,5 +66,6 @@ const sendSms = async (myPhoneNumber, message) =>{
 
 };
 
-    
+        
+//creating instace of function
 sendSms(myPhoneNumber, message);

b2d781fe7c442fd34c81451fe6c007cd3ecbb0d3
fawkeso16 <64199725+fawkeso16@users.noreply.github.com>
2024-05-29 19:20:47 +0100

    Update NPSMS.js
    Cangwed code to use clicksend api rather than twilio as twilio still not working

--- NPSMS.js
+++ NPSMS.js
@@ -1,17 +1,12 @@
-const express = require('express');
-const request = require('request');
+const fetch = require('node-fetch')
 
-
-request.get(body);
-
-const twilio = require('twilio');
-api.openweathermap.org/data/2.5/forecast?lat=12.57&lon=106.9&appid=331e13a2241a062eec05fdd320fad13b;
+const URL = 'api.openweathermap.org/data/2.5/forecast?lat=12.57&lon=106.9&appid=331e13a2241a062eec05fdd320fad13b'
 //code to request data using API weather
 
 
 
 //function to determine if weather is extreme
-function weatherupdate(){
+function weatherupdate(URL, ){
 
 }
 function extremeWeather(){
@@ -20,26 +15,48 @@ function extremeWeather(){
     }
 }
 ;
-//code to send a messgae to our phone number via the twilio number.
+
 
 // this is ollies authentication data, we only need one persons.
-const accountSid = 'AC14167b90a7b77edbc61ef3d0994f750e';
-const authToken = 'aaf42b244beec10f1ad8eea71be77965';
 
-const client = new twilio.Twilio(accountSid, authToken);
-
-const twilioPhoneNumber = '';
 const myPhoneNumber = '447401105231';
 const jamiePhoneNumber = '447575202880';
 const tyronePhoneNumber = '447939105014';
 const norbertPhoneNumber = '447592797548';
 
 
-//method to create message.//
-client.messages.create({
-    body: 'Hi',
-    from: twilioPhoneNumber,
-    to: myPhoneNumber
-  })
-  .then(message => console.log('Message sent. SID:', message.sid))
-  .catch(err => console.log('Error sending message:', err));
+
+message = 'This is a test, hopefully this works';
+
+
+//chnaged sms api service to clicksend, this async function 
+const sendSms = async (myPhoneNumber, message) =>{
+    const username = 'oliverfawkesg@gmail.com';
+    const apikey = '4B1D7004-C2C2-2CEA-D49E-5C8406E03346'
+
+    const data = {
+        messages: [
+        {
+            source: 'nodejs',
+            from:  'NPSMS Service',
+            body: message,
+            to: myPhoneNumber,
+        }
+        ]
+    };
+
+    const response = await fetch('https://rest.clicksend.com/v3/sms/send',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Basic ${Buffer.from(`{username}:${apiKey}`).toString(`base64`)}`
+    },
+    body: JSON.stringify(data)
+     });
+
+
+    const result = await response.json():
+    console.log("sms sent", result)
+
+
+

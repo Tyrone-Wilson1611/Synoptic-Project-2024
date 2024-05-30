a436ace1fb202f2d11cb4278c5b3c67be13249cf
fawkeso16 <64199725+fawkeso16@users.noreply.github.com>
2024-05-30 21:18:19 +0100

    Update NPSMS.mjs
    split daily checks and weather warnings into 2 seperte functions, also 2 seperate fetchs so i only fetch what is needed specificly for both. add messaging function into the wetaher checks so now should work and send a message with weather info to a single phone number

--- NPSMS.mjs
+++ NPSMS.mjs
@@ -5,33 +5,46 @@ import fetch from 'node-fetch';
 //just dont chnage the appid as this is a speific key.
 //cnt = 4 mean that only the next 4 (3h updates) are retrived (i overlooked before this which caused me hours of pain trying to get weathercheck function working)
 //tHing there may be a bug with the location
-const URL = "https://api.openweathermap.org/data/2.5/forecast?lat=12.57&lon=106.9&cnt=4&appid=331e13a2241a062eec05fdd320fad13b&units=metric";
+const dayForescastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=12.57&lon=106.9&cnt=4&appid=331e13a2241a062eec05fdd320fad13b&units=metric";
+const severeWeatherCheckURL = "https://api.openweathermap.org/data/2.5/forecast?lat=12.57&lon=106.9&cnt=1&appid=331e13a2241a062eec05fdd320fad13b&units=metric";
 
 //code to request data using API weather
 
-function getWeatherData(url) {
+function getDailyWeatherData(url) {
     fetch(url)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`error: ${error}`);}
             return response.json();
         }).then(weatherData => {
-            console.log(JSON.stringify(weatherData), null, 4)
+            //console.log(JSON.stringify(weatherData), null, 4)
             checks(weatherData.list); }).catch(error => {
             console.error('Error fetching weather data:', error);});
 }
-getWeatherData(URL)
+getDailyWeatherData(dayForescastURL)
+
+
+function getThreeHourlyWeatherData(url) {
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`error: ${error}`);}
+            return response.json();
+        }).then(weatherData => {
+            //console.log(JSON.stringify(weatherData), null, 4)
+            severeWeatherCheck(weatherData.list[0]); }).catch(error => {
+            console.error('Error fetching weather data:', error);});
+}
+getThreeHourlyWeatherData(severeWeatherCheckURL)
 
 
 //NOT DONT YET// but does work as intended. 
 // for this function so far, get some essential parameters, then find the avergage or expected vaulues and print to screen (just for testing purposes)
 // we will later all the SMS function from inside this function, passing through the message parameter as either a body or string formated however we feel.//
-// need to add extreme weather warning i.e. //
 function checks(forecast) {
     var hwindSpeed;
     var htemp;
     var hrainfall;
-    var time;
     var avgTemp = 0;
     var avgWindSpeed = 0;
     var expectedRainfall = 0;
@@ -40,10 +53,6 @@ function checks(forecast) {
         hrainfall = (update.rain && update.rain['3h']) ? update.rain['3h'] : 0; 
         htemp = update.main.temp;
         hwindSpeed = update.wind.speed; 
-        time = update.dt_txt;
-        if(hrainfall >= 50){ weatherWarning('Rain: ' + hrainfall + 'at :' + time)}
-        else if(htemp >= 35) { weatherWarning('Temp: ' + htemp + 'at :' + time)}
-        else if(hwindSpeed >= 30) { weatherWarning('Wind Speed: ' + hwindSpeed + 'at :' + time)}
         avgTemp += htemp;
         avgWindSpeed += hwindSpeed;
         expectedRainfall =+ hrainfall;
@@ -56,14 +65,32 @@ function checks(forecast) {
     expectedRainfall = Math.round(expectedRainfall);
 
     //testing//
-    console.log(avgTemp+' degrees Celsius : The average temp in the next 12 hours');
-    console.log(avgWindSpeed+'KPH : Average wind speed in the next 12 hours');
-    console.log(expectedRainfall +'mm of rain expected in the next 12 hours');
+     //console.log(avgTemp+' degrees Celsius : The average temp in the next 12 hours');
+     //console.log(avgWindSpeed+'KPH : Average wind speed in the next 12 hours');
+     //console.log(expectedRainfall +'mm of rain expected in the next 12 hours');
+
+    messageFunction(testPhoneNumber,'Temp: ' + avgTemp + '. Wind: ' + avgWindSpeed + '. Rain: ' + expectedRainfall);
+
 }
 
-function weatherWarning(warning){
-    console.log('Severe weather expected: ' + warning + 'Seek shelter immediately or prepare accordingly')
-
+function severeWeatherCheck(weather){
+  
+    var hrainfall = (weather.rain && weather.rain['3h']) ? weather.rain['3h'] : 0; 
+    var htemp = weather.main.temp;
+    var hwindSpeed = weather.wind.speed; 
+    var time = weather.dt_txt;
+    if(hrainfall >= 50){ 
+         messageFunction(testPhoneNumber,'Rain: ' + hrainfall + 'at :' + time);
+    }
+    else if(htemp >= 35) { 
+        messageFunction(testPhoneNumber,'Temp: ' + htemp + 'at :' + time);
+    }
+    else if(hwindSpeed >= 30) { 
+        messageFunction(testPhoneNumber, 'Wind Speed: ' + hwindSpeed + 'at :' + time);
+    }
+    else{
+        console.log('no severe warning for time: ' + time)
+    }
 }
 
 
@@ -71,44 +98,45 @@ const olliePhoneNumber = '447401105231';
 const jamiePhoneNumber = '447575202880';
 const tyronePhoneNumber = '447939105014';
 const norbertPhoneNumber = '447592797548';
+const testPhoneNumber = '61411111111';
 
 
-const message = 'This is a test, hopefully this works';
-
-
-//changed sms api service to clicksend, this async function takes in the phone number and message.
 // we have 2 options loop for every phone number, and send a message. or send a message to everyone at once (this would be harder i believe)
-//most of this code can be found on clicksend api docs, if you want to know or change look at https://developers.clicksend.com/docs/rest/v3/.
-// const messageFunction = async (PhoneNumber, message) =>{
-//     //ollies account authentiation details
-//     const username = 'ollieSMS';
-//     const apiKey = '4B1D7004-C2C2-2CEA-D49E-5C8406E03346'
-//     //can add a 'from' if we want(we should). 
-//     const data = {
-//         "messages": [
-//         {
-//             "to": myPhoneNumber,
-//             "source": 'sdk',
-//             "body": message
-//         }
-//         ]
-//     };
-//     const response = await fetch('https://rest.clicksend.com/v3/sms/send',{
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization': `Basic ${Buffer.from(`${username}:${apiKey}`).toString(`base64`)}`
-//     },
-//     body: JSON.stringify(data)
-//     });
+// most of this code can be found on clicksend api docs, if you want to know or change look at https://developers.clicksend.com/docs/rest/v3/.
+const messageFunction = async (phoneNumber, message) =>{
+    //ollies account authentiation details
+    const username = 'ollieSMS';
+    const apiKey = '4B1D7004-C2C2-2CEA-D49E-5C8406E03346'
+    //can add a 'from' if we want(we should). 
+    const data = {
+        "messages": [
+        {
+            "to": phoneNumber,
+            "source": 'sdk',
+            "body": message
+        }
+        ]
+    };
+    const response = await fetch('https://rest.clicksend.com/v3/sms/send',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Basic ${Buffer.from(`${username}:${apiKey}`).toString(`base64`)}`
+    },
+    body: JSON.stringify(data)
+    });
     
-//     if(!response.ok){
-//         const error = await response.json();
-//     console.error('Error', error)} 
-//     else{
-//         const result = await response.json();
-//     console.log("sms sent", result);}
-// };
+    if(!response.ok){
+        const error = await response.json();
+    console.error('Error', error)} 
+    else{
+        const result = await response.json();
+    console.log("sms sent", result);}
+};
 
 
-// messageFunction(olliePhoneNumber, message);
+//setInterval(() => getThreeHourlyWeatherData(severeWeatherCheckURL), 1000 * 60 * 60 * 3);
+//setInterval(() => getDailyWeatherData(dayForescastURL), 1000 * 60 * 60 * 24);
+
+
+//messageFunction(testPhoneNumber, message);

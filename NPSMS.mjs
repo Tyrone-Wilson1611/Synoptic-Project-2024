4d8984f00d6ff7709bb5c39aac5d5a4682115c46
fawkeso16 <64199725+fawkeso16@users.noreply.github.com>
2024-05-31 14:03:01 +0100

    Update NPSMS.mjs
    cleaned up code, added todolist, added docs to find code examples at top of page

--- NPSMS.mjs
+++ NPSMS.mjs
@@ -1,14 +1,20 @@
+//Things to do 
+// 1 - write the code to send messages to every phone number in a data structure.
+// 2 - change the weather data retrievel from an array to a hasmap to aid in use in other functions (luxury not needed, can slightly make code more efficient if we reuse it alot whoch we arent at the moment)
+// 3 - write a funtion to decrypt phone numbers. not exactly sure on how to do this. i guess we encrypt on the DBMS and decrypt on the backend
+
+//notes 
+// if you want to alter or add any specifics go to openweather api documentation and go to 5 day/3 houlry api, there it will tell you any attributes you can add to the data.
+// send sms function is not in the form we should have as final produc it does what we want but not how we want, we need to change to send to multiple people ( this can be done serveral ways).
+// most of the code for send sms can be found on clicksend api docs, if there are any errors or if you want to know or change the code look at https://developers.clicksend.com/docs/rest/v3/.
+
+
 import fetch from 'node-fetch';
 
-//if you want to alter or add any specifics go to openweather api documentation and go to 5 day/3 houlry api, there it will tell you any attributes you can add to the data.
-//This url will open a page that returns weather info for location lat 12.57 and long 106.9 which is the village hall of pu ngaol i believe, if im wrong feel free to change you can test by putting into your web broweser.
-//just dont chnage the appid as this is a speific key.
-//cnt = 4 mean that only the next 4 (3h updates) are retrived (i overlooked before this which caused me hours of pain trying to get weathercheck function working)
-//tHing there may be a bug with the location
+
 const dayForescastURL = "https://api.openweathermap.org/data/2.5/forecast?lat=12.57&lon=106.9&cnt=4&appid=331e13a2241a062eec05fdd320fad13b&units=metric";
 const severeWeatherCheckURL = "https://api.openweathermap.org/data/2.5/forecast?lat=12.57&lon=106.9&cnt=1&appid=331e13a2241a062eec05fdd320fad13b&units=metric";
 
-//code to request data using API weather
 
 function getDailyWeatherData(url) {
     fetch(url)
@@ -39,7 +45,7 @@ getThreeHourlyWeatherData(severeWeatherCheckURL)
 
 
 //NOT DONT YET// but does work as intended. 
-// for this function so far, get some essential parameters, then find the avergage or expected vaulues and print to screen (just for testing purposes)
+// for this function so far, get some essential properties, then find the avergage or expected vaulues and print to screen (just for testing purposes)
 // we will later all the SMS function from inside this function, passing through the message parameter as either a body or string formated however we feel.//
 function checks(forecast) {
     var hwindSpeed;
@@ -65,11 +71,11 @@ function checks(forecast) {
     expectedRainfall = Math.round(expectedRainfall);
 
     //testing//
-     //console.log(avgTemp+' degrees Celsius : The average temp in the next 12 hours');
-     //console.log(avgWindSpeed+'KPH : Average wind speed in the next 12 hours');
-     //console.log(expectedRainfall +'mm of rain expected in the next 12 hours');
+    // console.log(avgTemp+' degrees Celsius : The average temp in the next 12 hours');
+    // console.log(avgWindSpeed+'KPH : Average wind speed in the next 12 hours');
+    // console.log(expectedRainfall +'mm of rain expected in the next 12 hours');
 
-     messageFunction(testPhoneNumber,'Nu Pgoal Daily weather service. Average temperature today: ' + avgTemp + '. Average wind speed : ' + avgWindSpeed + '. And expected rainfall is around: ' + expectedRainfall);
+     messageFunction(testPhoneNumber,'Nu Pgoal Daily weather update. Average temperature today: ' + avgTemp + '°C  .Average wind speed : ' + avgWindSpeed + ' KPH .And expected rainfall is around: ' + expectedRainfall +'mm. Any other properties we want ot add.....' );
 
 }
 
@@ -101,8 +107,6 @@ const norbertPhoneNumber = '447592797548';
 const testPhoneNumber = '61411111111';
 
 
-// we have 2 options loop for every phone number, and send a message. or send a message to everyone at once (this would be harder i believe)
-// most of this code can be found on clicksend api docs, if you want to know or change look at https://developers.clicksend.com/docs/rest/v3/.
 const messageFunction = async (phoneNumber, message) =>{
     //ollies account authentiation details
     const username = 'ollieSMS';
@@ -112,6 +116,7 @@ const messageFunction = async (phoneNumber, message) =>{
         "messages": [
         {
             "to": phoneNumber,
+           // "from": 'Pu Ngaol SMS service',
             "source": 'sdk',
             "body": message
         }
@@ -132,6 +137,7 @@ const messageFunction = async (phoneNumber, message) =>{
     else{
         const result = await response.json();
     console.log("sms sent", result);}
+    console.log(JSON.stringify(data))
 };
 
 

b3c464efd14655ebca07adbcea8624192e6f200d
fawkeso16 <64199725+fawkeso16@users.noreply.github.com>
2024-06-02 03:06:11 +0100

    Update message.mjs
    added function to send to single phone number, mostly for testing

--- message.mjs
+++ message.mjs
@@ -1,6 +1,4 @@
-// most of the code for send sms can be found on clicksend api docs, if there are any errors or if you want to know or change the code look at https://developers.clicksend.com/docs/rest/v3/.
-
-import {getallPhoneNums} from '/encryption'
+import {getallPhoneNums} from './database.mjs';
 
 const olliePhoneNumber = '447401105231';
 const jamiePhoneNumber = '447575202880';
@@ -8,8 +6,7 @@ const tyronePhoneNumber = '447939105014';
 const norbertPhoneNumber = '447592797548';
 const testPhoneNumber = '61411111111';
 
-
-const messageFunction = async (message) => {
+export const messageFunction = async (message) => {
     // Ollie's account authentication details
     const username = 'ollieSMS';
     const apiKey = '4B1D7004-C2C2-2CEA-D49E-5C8406E03346';
@@ -42,14 +39,44 @@ const messageFunction = async (message) => {
         } else {
             const result = await response.json();
             console.log(result);
+           // console.log(JSON.stringify(result))
         }
     });
 };
 
-//setInterval(() => getThreeHourlyWeatherData(severeWeatherCheckURL), 1000 * 60 * 60 * 3);
-//setInterval(() => getDailyWeatherData(dayForescastURL), 1000 * 60 * 60 * 24);
+export const singleMessageFunction = async (message) => {
+    // Ollie's account authentication details
+    const username = 'ollieSMS';
+    const apiKey = '4B1D7004-C2C2-2CEA-D49E-5C8406E03346';
+    // Can add a 'from' if we want (we should).
+        const data = {
+            "messages": [
+                {
+                    "to": testPhoneNumber,
+                    // "from": 'Pu Ngaol SMS service',
+                    "source": 'sdk',
+                    "body": message
+                }
+            ]
+        };
+    
+        const response = await fetch('https://rest.clicksend.com/v3/sms/send', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Basic ${Buffer.from(`${username}:${apiKey}`).toString(`base64`)}`
+            },
+            body: JSON.stringify(data)
+        });
+    
+        if (!response.ok) {
+            const error = await response.json();
+            console.log(error);
+        } else {
+            const result = await response.json();
+            console.log(result);
+           // console.log(JSON.stringify(result))
 
+        }
+};
 
-//extra tests
-//messageFunction(testPhoneNumber, message);
-//messageFunction()

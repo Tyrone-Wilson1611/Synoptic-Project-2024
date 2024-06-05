ee7ad281e4a5ad9bb840aedd027e1796b7a939e7
fawkeso16 <64199725+fawkeso16@users.noreply.github.com>
2024-06-05 02:17:00 +0100

    Update message.mjs
    Changed code alot, added extra logging and simpler implementation

--- message.mjs
+++ message.mjs
@@ -1,82 +1,86 @@
-import {getallPhoneNums} from './database.mjs';
+import fetch from 'node-fetch'; // Ensure you have node-fetch installed
 
 const olliePhoneNumber = '447401105231';
-const jamiePhoneNumber = '447575202880';
+const jamiePhoneNumber = '07575202880';
 const tyronePhoneNumber = '447939105014';
 const norbertPhoneNumber = '447592797548';
 const testPhoneNumber = '61411111111';
 
-export const messageFunction = async (message) => {
-    // Ollie's account authentication details
+export const messageFunction = async (message, allphonenum) => {
+    if (allphonenum.length == 0) {
+        console.log('empty');
+        return;
+    }
+    const nums = allphonenum;
     const username = 'ollieSMS';
     const apiKey = '4B1D7004-C2C2-2CEA-D49E-5C8406E03346';
-    // Can add a 'from' if we want (we should).
 
-    getallPhoneNums.forEach(async phoneNumber => {
+    for (const phoneNumber of nums) {
         const data = {
             "messages": [
                 {
                     "to": phoneNumber,
-                    // "from": 'Pu Ngaol SMS service',
                     "source": 'sdk',
                     "body": message
                 }
             ]
         };
-    
+
+
+        //code example from clicksend api docs
         const response = await fetch('https://rest.clicksend.com/v3/sms/send', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Basic ${Buffer.from(`${username}:${apiKey}`).toString(`base64`)}`
+                'Authorization': `Basic ${Buffer.from(`${username}:${apiKey}`).toString('base64')}`
             },
             body: JSON.stringify(data)
         });
-    
+
         if (!response.ok) {
             const error = await response.json();
-            console.log(error);
+            console.log(error)
         } else {
             const result = await response.json();
-            console.log(result);
-           // console.log(JSON.stringify(result))
+            console.log(result)
         }
-    });
+    }
 };
 
 export const singleMessageFunction = async (message) => {
-    // Ollie's account authentication details
     const username = 'ollieSMS';
     const apiKey = '4B1D7004-C2C2-2CEA-D49E-5C8406E03346';
-    // Can add a 'from' if we want (we should).
-        const data = {
-            "messages": [
-                {
-                    "to": testPhoneNumber,
-                    // "from": 'Pu Ngaol SMS service',
-                    "source": 'sdk',
-                    "body": message
-                }
-            ]
-        };
-    
-        const response = await fetch('https://rest.clicksend.com/v3/sms/send', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Basic ${Buffer.from(`${username}:${apiKey}`).toString(`base64`)}`
-            },
-            body: JSON.stringify(data)
-        });
-    
-        if (!response.ok) {
-            const error = await response.json();
-            console.log(error);
-        } else {
-            const result = await response.json();
-            console.log(result);
-           // console.log(JSON.stringify(result))
 
-        }
+    const data = {
+        "messages": [
+            {
+                "to": testPhoneNumber,
+                "source": 'sdk',
+                "body": message
+            }
+        ]
+    };
+
+    const response = await fetch('https://rest.clicksend.com/v3/sms/send', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Basic ${Buffer.from(`${username}:${apiKey}`).toString('base64')}`
+        },
+        body: JSON.stringify(data)
+    });
+
+    if (!response.ok) {
+        const error = await response.json();
+        console.log(error)
+    } else {
+        const result = await response.json();
+        console.log(result)
+    }
 };
 
+
+//  singleMessageFunction('test');
+
+//  var testallPhoneNums = ['447401105231',testPhoneNumber];
+//  messageFunction('test', testallPhoneNums);

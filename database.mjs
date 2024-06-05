b43a9e58be360684a87346d2a3c71bd566847756
fawkeso16 <64199725+fawkeso16@users.noreply.github.com>
2024-06-05 02:49:08 +0100

    Update database.mjs
    made it so adding phone numbers encrypts them

--- database.mjs
+++ database.mjs
@@ -1,37 +1,48 @@
 //whoever makes database needs to put there info into connect variable, if confused look here https://node-postgres.com/apis/client 
 import pg from 'pg';
-import { encryptPhoneNumber, decryptPhoneNumber, getPhone } from './encryption.mjs';
+import { encryptPhoneNumber, getKey} from './encryption.mjs';
+import { encode } from 'querystring';
 
 // Database connection setup
-const client = new pg.Client({
-    user: '',
-    password: '',
-    host: '',
-    port: '',
-    database: '',
-});
+// const client = new pg.Client({
+//     user: '',
+//     password: '',
+//     host: '',
+//     port: '',
+//     database: '',
+// });
 
-client.connect(err => {
-    if (err) {
-        console.error('error', err);
-    }
-});
+// client.connect(err => {
+//     if (err) {
+//         console.error('error', err);
+//     }
+// });
 
-//could make this an ID select, or just get any
-async function getallPhoneNums() {
-    var allPhoneNums = [];
-    const res = await client.query('SELECT PhoneNumber FROM person');
-    res.rows.forEach(row => {
-        const decryptedPhoneNumber = decryptPhoneNumber(row.phonenumber);
-        allPhoneNums.push(decryptPhoneNumber);
-    });
+var allPhoneNums = [];
+
+export async function getallPhoneNums() {
     return allPhoneNums;
-
 }
 
-async function insertPerson(name, phoneNumber) {
-    const encryptedPhoneNumber = encryptPhoneNumber(phoneNumber);
-    const query = 'insert;;
-    const info = [name, encryptedPhoneNumber];
-    await client.query(query, info);
+export function insertphonenum(phoneNumber) {
+    var encryptedPhoneNumber = encryptPhoneNumber(phoneNumber, getKey());
+    if (!allPhoneNums.includes(encryptedPhoneNumber)) { 
+        allPhoneNums.push(encryptedPhoneNumber);
+        console.log('Phone number added');
+    } else {
+        console.log('Phone number exists');
+    }
 }
+
+// export async function insertPerson(name, phoneNumber) {
+//     const encryptedPhoneNumber = encryptPhoneNumber(phoneNumber);
+//     if (!allPhoneNums.includes(phoneNumber)) { 
+//         allPhoneNums.push(phoneNumber);
+//         const query = 'INSERT INTO person (name, phonenumber) VALUES ($1, $2)';
+//         const info = [name, encryptedPhoneNumber];
+//         await client.query(query, info);
+//     } else {
+//         console.log('Phone number exists');
+//     }
+// }
+

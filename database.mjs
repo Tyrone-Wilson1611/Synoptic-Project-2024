0b8996dac263373b28c591fd7b294f044e88a5c7
fawkeso16 <64199725+fawkeso16@users.noreply.github.com>
2024-06-13 04:30:39 +0100

    Update database.mjs
    final draft

--- database.mjs
+++ database.mjs
@@ -1,31 +1,42 @@
 //whoever makes database needs to put there info into connect variable, if confused look here https://node-postgres.com/apis/client 
 import pg from 'pg';
-import { encryptPhoneNumber, getKey} from './encryption.mjs';
-import { encode } from 'querystring';
+import { encryptPhoneNumber, ourKey} from './encryption.mjs';
 
-// Database connection setup
-// const client = new pg.Client({
-//     user: '',
-//     password: '',
-//     host: '',
-//     port: '',
-//     database: '',
-// });
-
-// client.connect(err => {
-//     if (err) {
-//         console.error('error', err);
-//     }
-// });
 
 var allPhoneNums = [];
 
-export async function getallPhoneNums() {
+
+// const client = new pg.Client({
+//     user: 'fbh23equ',
+//     password: 'ByGentlemanShoe49*',
+//     host: 'cmpstudb-01.cmp.uea.ac.uk',
+//     port: '5432',
+//     database: 'fbh23equ',
+//     connectionTimeoutMillis: 5000 
+// });
+
+
+// async function connectAndSetSchema() {
+//     try {
+//         await client.connect();
+//         console.log('Connected to the database');
+//         await client.query('SET search_path TO project, public;');
+//         console.log('Search path set to project schema');
+//     } catch (err) {
+//         console.error('Connection error:', err.stack);
+//     }
+// }
+// connectAndSetSchema();
+
+export async function getallPhoneNumsFromDatabase() {
+    const res = await client.query('SELECT "PhoneNumber" FROM "Users"');
+    allPhoneNums  = res.rows.map(row => row.PhoneNumber);
     return allPhoneNums;
-}
+    }
+
 
 export function insertphonenum(phoneNumber) {
-    var encryptedPhoneNumber = encryptPhoneNumber(phoneNumber, getKey());
+    var encryptedPhoneNumber = encryptPhoneNumber(phoneNumber, ourKey);
     if (!allPhoneNums.includes(encryptedPhoneNumber)) { 
         allPhoneNums.push(encryptedPhoneNumber);
         console.log('Phone number added');
@@ -34,15 +45,67 @@ export function insertphonenum(phoneNumber) {
     }
 }
 
-// export async function insertPerson(name, phoneNumber) {
-//     const encryptedPhoneNumber = encryptPhoneNumber(phoneNumber);
-//     if (!allPhoneNums.includes(phoneNumber)) { 
-//         allPhoneNums.push(phoneNumber);
-//         const query = 'INSERT INTO person (name, phonenumber) VALUES ($1, $2)';
-//         const info = [name, encryptedPhoneNumber];
-//         await client.query(query, info);
-//     } else {
-//         console.log('Phone number exists');
-//     }
-// }
 
+export async function insertUser(id, name, phoneNumber, address, signUpdDate) {
+    const encryptedPhoneNumber = encryptPhoneNumber(phoneNumber);
+    if (!allPhoneNums.includes(encryptedPhoneNumber)) { 
+        allPhoneNums.push(encryptedPhoneNumber);
+        const query = 'insert person (id, name, encryptedPhoneNumber, address, signup-date) VALUES ($1, $2, $3, $4, $5)';
+        const info = [id, name, encryptedPhoneNumber, address, signUpdDate];
+        await client.query(query, info);
+    } else {
+        console.log('Phone number exists');
+    }
+}
+
+
+
+export async function insertWeatherCondition(conditionID, conditionType, description) {
+    const query = 'INSERT INTO "WeatherConditions" ("ConditionID", "ConditionType", "Description") VALUES ($1, $2, $3)';
+    const info = [conditionID, conditionType, description];
+    try {
+        await client.query(query, info);
+        console.log('Weather condition inserted');
+    } catch (err) {
+        console.error('Insert error:', err.stack);
+    }
+}
+
+
+
+export async function insertAlert(alertID, conditionID, alertMessage) {
+    const query = 'INSERT INTO Alerts (alertID, ConditionID, AlertMessage) VALUES ($1, $2, $3)';
+    const info = [alertID, conditionID, alertMessage];
+    try{
+         await client.query(query, info);
+        console.log('Alert inserted');
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+export async function insertUserAlert(userID, alertID) {
+    const query = 'INSERT INTO UserAlerts (UserID, AlertID) VALUES ($1, $2)';
+    const info = [userID, alertID];
+    await client.query(query, info);
+    console.log('User alert inserted');
+}
+
+export async function insertWeatherStation(stationName, location) {
+    const query = 'INSERT INTO WeatherStations (StationName, Location) VALUES ($1, $2)';
+    const info = [stationName, location];
+    await client.query(query, info);
+    console.log('Weather station inserted');
+}
+
+export async function insertWeatherData(stationID, conditionID, temperature, rain, windSpeed) {
+    const query = 'INSERT INTO WeatherData (StationID, ConditionID, Temperature, rain, WindSpeed) VALUES ($1, $2, $3, $4, $5)';
+    const info = [stationID, conditionID, temperature, rain, windSpeed];
+    await client.query(query, info);
+    console.log('Weather data inserted');
+}
+
+export async function getallPhoneNums() {
+    return allPhoneNums;
+}
